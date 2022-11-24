a6f1f4596a8c750723b1f1c63442a2f019a680a8
theautomation <github@theautomation.nl>
2022-11-25 00:13:06 +0100

    Add workdir

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 # constants
 BRIDGE="protonmail-bridge --cli"
-BRIDGE_IMAP_PORT="1143"
-BRIDGE_SMTP_PORT="1025"
-GPG_PARAMS="${HOME}/gpg-key-parameters"
+GPG_PARAMS="/home/gpg-key-parameters"
 FIFO="/tmp/fifo"
 # other variables are coming from the environment itself
 
 # main
-if ! [ -d ~/.gnupg ]; then
+if ! [ -d /home/gnupg ]; then
     gpg --generate-key --batch ${GPG_PARAMS}
 fi
 
-if ! [ -d ~/.password-store ]; then
+if ! [ -d /home/.password-store ]; then
     pass init "$(awk -F: '/^Name-Real/ {print $2}' ${GPG_PARAMS})"
 fi
 
-if ! [ -f ~/.cache/protonmail/bridge ]; then
+if ! [ -f /home/.cache/protonmail/bridge ]; then
     echo -e "login\n${PROTONMAIL_LOGIN}\n${PROTONMAIL_PASSWORD}\n${PROTONMAIL_EXTRA_2FA}" | ${BRIDGE} ${BRIDGE_EXTRA_ARGS}
 fi
 
@@ -27,10 +25,11 @@ if [ "${BRIDGE_SPLIT_ADDRESS_MODE}" = true ]; then
     echo -e "change mode\nyes\n" | ${BRIDGE} ${BRIDGE_EXTRA_ARGS}
 fi
 
-# socat will make the connection appear to come from 127.0.0.1, since
-# the ProtonMail Bridge expects that
-socat TCP-LISTEN:${SMTP_PORT},fork TCP:127.0.0.1:${BRIDGE_SMTP_PORT} &
-socat TCP-LISTEN:${IMAP_PORT},fork TCP:127.0.0.1:${BRIDGE_IMAP_PORT} &
+# socat will make the conn appear to come from 127.0.0.1
+# ProtonMail Bridge currently expects that.
+# It also allows us to bind to the real ports :)
+socat TCP-LISTEN:25,fork TCP:127.0.0.1:1025 &
+socat TCP-LISTEN:143,fork TCP:127.0.0.1:1143 &
 
 # display account information, then keep stdin open
 [ -e ${FIFO} ] || mkfifo ${FIFO}
